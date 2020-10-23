ecf0dd546af3835796a6fa92f2f45a9c2c99b78e
Frederik <34657773+flja@users.noreply.github.com>
2020-10-23 10:39:51 +0200

    1

---
+++ Lector06/OOP06/OOP06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(0);
+            list.Add(5);
+            list.Add(4);
+            list.Add(2);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class HandyMethods
+    {
+        public T Min<T>(List<T> l) where T:IComparable
+        {
+            return (T)Convert.ChangeType(0, typeof(T));
+        }
+
+        public T Max<T>(List<T> l) where T : IComparable
+        {
+            return (T)Convert.ChangeType(0, typeof(T));
+        }
+    }
+}
